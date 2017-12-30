32ee2e9c34b3c25fbc1aaf8248099f37ddd115d5
RicardoSerna1998 <ricki1998@outlook.com>
2017-12-30 09:49:21 -0600

    Commit

--- app/src/main/java/com/example/ricardosernam/puntodeventa/MainActivity.java
+++ app/src/main/java/com/example/ricardosernam/puntodeventa/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Ventas) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.Compras) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.Productos) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.Vendedores) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.Provedores) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.Clientes) {
 
         }
 
