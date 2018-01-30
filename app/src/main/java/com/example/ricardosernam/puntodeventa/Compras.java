951bbf22e188ab4a359c83b4c6ace07c08b86249
martinrdzserna <martinrdzserna@hotmail.com>
2018-01-29 21:48:38 -0600

    Commit

--- app/src/main/java/com/example/ricardosernam/puntodeventa/Compras.java
+++ app/src/main/java/com/example/ricardosernam/puntodeventa/Compras.java
@@ -1,5 +1,6 @@
 package com.example.ricardosernam.puntodeventa;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,13 +9,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.zxing.Result;
 
-public class Compras extends Fragment {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+
+public class Compras extends Fragment implements ZXingScannerView.ResultHandler{
+private ZXingScannerView escanerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_compras, container, false);
+    }
+
+
+    public void BtnEscanearQR(View view){
+        escanerView=new ZXingScannerView(getContext());
+        escanerView.setResultHandler(this);
+        escanerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        escanerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Resultado del escaner");
+        builder.setMessage("Resultado " + result.getText() + "\n" + "Formato "+result.getBarcodeFormat());
+        AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+        escanerView.resumeCameraPreview(this);
+
+
+
     }
 }
