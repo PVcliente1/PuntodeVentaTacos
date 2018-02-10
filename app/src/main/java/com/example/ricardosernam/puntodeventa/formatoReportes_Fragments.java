1fd27422f6fb3018134aa6669ca34627907b8b41
RicardoSerna1998 <ricki1998@outlook.com>
2018-02-10 00:13:07 -0600

    Por fin pude con Reportes

--- app/src/main/java/com/example/ricardosernam/puntodeventa/formatoReportes_Fragments.java
+++ app/src/main/java/com/example/ricardosernam/puntodeventa/formatoReportes_Fragments.java
@@ -1,5 +1,6 @@
 package com.example.ricardosernam.puntodeventa;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,11 +12,17 @@ import android.widget.Toast;
  * Created by Ricardo Serna M on 08/02/2018.
  */
 
+@SuppressLint("ValidFragment")
 public class formatoReportes_Fragments extends android.support.v4.app.Fragment{
+    public String tipo;
+    /*@SuppressLint("ValidFragment")
+    public formatoReportes_Fragments(String tipo){
+        this.tipo=tipo;
+    }*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.plantilla_reportes, container, false);
-        Toast.makeText(getActivity(), "Somos ", Toast.LENGTH_SHORT).show();
+
         return view;
     }
 }
