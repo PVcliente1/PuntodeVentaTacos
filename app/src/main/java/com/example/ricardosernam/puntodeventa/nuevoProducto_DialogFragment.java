73b114a4dc6bbe69f2de1580de9078dab76c330a
RicardoSerna1998 <ricki1998@outlook.com>
2018-02-13 22:21:43 -0600

    Completo traer imagen del producto

--- app/src/main/java/com/example/ricardosernam/puntodeventa/nuevoProducto_DialogFragment.java
+++ app/src/main/java/com/example/ricardosernam/puntodeventa/nuevoProducto_DialogFragment.java
@@ -1,12 +1,15 @@
 package com.example.ricardosernam.puntodeventa;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,14 +24,9 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-/**
- * Created by Ricardo Serna M on 06/02/2018.
- */
-
 public class nuevoProducto_DialogFragment extends DialogFragment {
     private EditText nombreP, precio, codigo;
     private Button aceptarM, cancelarM, imagen, escanear;
-    //private android.support.v4.app.FragmentManager manejador = getSupportFragmentManager();  //manejador que permite hacer el cambio de ventanas
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,56 +66,77 @@ public class nuevoProducto_DialogFragment extends DialogFragment {
         imagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirGaleria(view);
+                final AlertDialog.Builder imagenProducto= new AlertDialog.Builder(getActivity());
+                imagenProducto.setTitle("Imagen");
+                imagenProducto.setMessage("Seleccion como quieres traer tu imagen");
+                imagenProducto.setCancelable(false);
+                imagenProducto.setPositiveButton("Desde galeria...", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface tipoDescuento, int id) {
+                       Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        gallery.setType("image/*");
+                        startActivityForResult(Intent.createChooser(gallery, "Seleccione una imagen"), 1);
+                        tipoDescuento.dismiss();
+                    }
+                });
+                imagenProducto.setNegativeButton("Tomar foto", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface tipoDescuento, int id) {
+                        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(i, 3);
+                        tipoDescuento.dismiss();
+                    }
+                });
+                imagenProducto.show();
             }
         });
         getDialog().setTitle("Nuevo Producto");
         return rootView;
     }
-    public void abrirGaleria(View v){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Seleccione una imagen"),
-                1);
-    }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        ////1 para entrar a galeria y tomar una foto
+        if (requestCode == 1) {
+            Uri selectedImage;
+            if (resultCode == Activity.RESULT_OK) {
+                selectedImage = data.getData();
+                String selectedPath=selectedImage.getPath();
+                if (requestCode == 1) {
+
+                    if (selectedPath != null) {
+                        InputStream imageStream = null;
+                        try {
+                            imageStream = getActivity().getContentResolver().openInputStream(selectedImage);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Transformamos la URI de la imagen a inputStream y este a un Bitmap
+                        Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+
+                        // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
+                        ImageView mImg = (ImageView) getView().findViewById(R.id.ImgImagen);
+                        mImg.setImageBitmap(bmp);
+                        Toast.makeText(getActivity(), selectedPath, Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+
+        }
         ///2 para escanear
         if (requestCode == 2 && data != null) {
             //obtener resultados
             codigo.setText(data.getStringExtra("BARCODE"));
         }
-        if (requestCode == 1) {
-            Uri selectedImageUri = null;
-            Uri selectedImage;
-            if (resultCode == Activity.RESULT_OK) {
-                        selectedImage = data.getData();
-                        String selectedPath=selectedImage.getPath();
-                        if (requestCode == 1) {
-
-                            if (selectedPath != null) {
-                                InputStream imageStream = null;
-                                try {
-                                    imageStream = getActivity().getContentResolver().openInputStream(
-                                            selectedImage);
-                                } catch (FileNotFoundException e) {
-                                    e.printStackTrace();
-                                }
-
-                                // Transformamos la URI de la imagen a inputStream y este a un Bitmap
-                                Bitmap bmp = BitmapFactory.decodeStream(imageStream);
-
-                                // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
-                                ImageView mImg = (ImageView) getView().findViewById(R.id.ImgImagen);
-                                mImg.setImageBitmap(bmp);
-
-                            }
-                        }
-                    }
-
+        //3 Captura de foto
+        if(requestCode == 3) {
+            if(data != null) {
+                ImageView imagen = getView().findViewById(R.id.ImgImagen);
+                imagen.setImageBitmap((Bitmap) data.getParcelableExtra("data"));
             }
+            else{
+            }
+        }
         }
 
 }
