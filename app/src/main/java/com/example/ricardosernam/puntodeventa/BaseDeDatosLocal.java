f5ef9b5572b4541de66c4d97f0c9a1ef90cef1ef
daniel-linux <daniel_ori@live.com>
2018-02-15 20:48:33 -0600

    Bases de datos

--- app/src/main/java/com/example/ricardosernam/puntodeventa/BaseDeDatosLocal.java
+++ app/src/main/java/com/example/ricardosernam/puntodeventa/BaseDeDatosLocal.java
@@ -12,11 +12,13 @@ public class BaseDeDatosLocal extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE Proveedores(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Apellidos TEXT, Telefono TEXT, Direccion TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE Usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Contrasena TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("CREATE TABLE Proveedores(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Apellidos TEXT, Telefono TEXT, Direccion TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE Usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Contrasena TEXT)");
     }
 }
